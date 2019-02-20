c5138b86e543212dccc30a23dfcb1b349a5e8f2b
Karol Jasik <hiroka322@gmail.com>
2019-02-20 17:10:19 +0100

    break into 2 classes

--- src/vsc-translate-snippets.ts
+++ src/vsc-translate-snippets.ts
@@ -1,10 +1,14 @@
 // modules
 import jadeTranslator from "./translators/jade";
+import jadeData from "./translators/jadeData";
 
-class VSCTranslateSnippets{
-    public jade(options){
-        const jade = new jadeTranslator(options);
-        jade.start();
+class VSCTranslateSnippets {
+    public jade(options) {
+        const JadeData = new jadeData(options);
+        const source = JadeData.getFile;
+        const JadeTranslate = new jadeTranslator(source);
+
+        JadeData.save(JadeTranslate.getHtml);
     }
 }
 
